7693fcdb3cc1a34832449e737f73433f51e7cb19
wagneracabral <161398182+wagneracabral@users.noreply.github.com>
2024-02-28 19:12:24 -0300

    Update Aula10.java

--- AulaPoo/src/br/com/senai/Aula10.java
+++ AulaPoo/src/br/com/senai/Aula10.java
@@ -6,14 +6,58 @@ public class Aula10 {
 
 	public static void main(String[] args) {
 
+		int opcaomesario = 0;
 		int opcao = 0;
+		int totalangela = 0;
+		int totalgean = 0;
+		int totalcesar = 0;
+		int totalnulo = 0;
+		int totalbranco =0;
+
+
 		do {
-			opcao = Integer.parseInt(JOptionPane.showInputDialog("Digite uma opção -1)Cadastrar Aluno, \n2)Cadastrar Professor,9)Sair do programa"));
-			if(código ==1) {
-				System.out.println("Nome: Angela Pepino \n Partido: PMDB");
+			opcaomesario = Integer.parseInt(JOptionPane.showInputDialog(" Opcoes Mesario:\n 1)Iniciar Votação \n 2) Encerrar"));
+			if(opcaomesario ==1) {
+				do {
+					opcao = Integer.parseInt(JOptionPane.showInputDialog("Escolha um candidato: \n-1)Angela Pepino\n*Partido: PMDB \n 2)Gean da Silva \n *Partido: PT \n 3)Cesar Souza Neto \n *Partido: DEM \n 4)Nome: Voto Nulo \n 5)Voto em Branco"));
+					if(opcao ==1) {
+						System.out.println("Nome: Angela Pepino \n *Partido: PMDB");
+						totalangela++;
+						break;
+					}
+					if(opcao ==2) {
+						System.out.println("Gean da Silva \\n *Partido: PT ");
+						totalgean++;
+						break;
+					}
+					if(opcao ==3) {
+						System.out.println("Cesar Souza Neto \\n *Partido: DEM");
+						totalcesar++;
+						break;
+					}
+					if(opcao ==4) {
+						System.out.println("Nome: Voto Nulo");
+						totalnulo++;
+						break;
+					}
+					if(opcao ==5) {
+						System.out.println("Nome: Voto em Branco");
+						totalbranco++;
+						break;
+
+					}
+				}
+				while (opcao != 5);
 			}
-			if(codigo ==2) {
-				System.out.println("Cadastrar Professor");
+		}while (opcaomesario != 2);
+
+		System.out.println("\nVotacao Encerrada.");
+		System.out.println("\nContagem dos Votos:");
+		System.out.println("Angela Pepino - PMDB: " + totalangela);
+		System.out.println("Gean da Silva - PT: " + totalgean);
+		System.out.println("Cesar Souza Neto - DEM: " + totalcesar);
+		System.out.println("Nulo: " + totalnulo);
+		System.out.println("Branco: " + totalbranco);
 	}
 
 }
