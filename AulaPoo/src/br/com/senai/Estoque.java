5f417dbe9b391dd56b0d92d3df37162266d1d188
wagneracabral <161398182+wagneracabral@users.noreply.github.com>
2024-03-11 21:53:32 -0300

    Add files via upload

---
+++ AulaPoo/src/br/com/senai/Estoque.java
@@ -0,0 +1,44 @@
+package br.com.senai;
+
+public class Estoque {
+
+	private int codprod;
+	private String nomeprod;
+	private String descprod;
+	private int qtdprod;
+	private double precoprod;
+	public int getCodprod() {
+		return codprod;
+	}
+	public void setCodprod(int codprod) {
+		this.codprod = codprod;
+	}
+	public String getNomeprod() {
+		return nomeprod;
+	}
+	public void setNomeprod(String nomeprod) {
+		this.nomeprod = nomeprod;
+	}
+	public String getDescprod() {
+		return descprod;
+	}
+	public void setDescprod(String descprod) {
+		this.descprod = descprod;
+	}
+	public int getQtdprod() {
+		return qtdprod;
+	}
+	public void setQtdprod(int qtdprod) {
+		this.qtdprod = qtdprod;
+	}
+	public double getPrecoprod() {
+		return precoprod;
+	}
+	public void setPrecoprod(double precoprod) {
+		this.precoprod = precoprod;
+	}
+	
+	
+	
+	
+}
